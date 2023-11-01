098f9fbaf74ad6b5def30cc231d4f9aaab20255a
Nate <99703235+GreatNateDev@users.noreply.github.com>
2023-11-01 16:45:28 -0500

    this is funni

---
+++ slang.h
@@ -0,0 +1,15 @@
+#ifndef GENZ_SLANG_H
+#include <iostream>
+#define rizz if
+#define fam cout
+#define bussin using namespace std
+#define canceled break
+#define W int
+#define dank char
+#define ghosted string
+#define salty return
+#define cap switch
+#define drip endl
+#define sussin else
+#define bet cin
+#endif
